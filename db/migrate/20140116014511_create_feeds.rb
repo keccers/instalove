b913d4dc6e46c4594d733845118a292662db3b53
Katherine Champagne <kechampagne@gmail.com>
2014-01-21 01:07:59 -0500

    Change association type between photos and feeds to HABTM

--- db/migrate/20140116014511_create_feeds.rb
+++ db/migrate/20140116014511_create_feeds.rb
@@ -2,7 +2,8 @@ class CreateFeeds < ActiveRecord::Migration
   def change
     create_table :feeds do |t|
     	t.string :title
-    	
+    	t.text :description
+    	t.integer :user_id
     	t.timestamps
     end
   end
