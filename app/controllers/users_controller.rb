2102f21d4b033bd75cd328c5793674776fa0745d
Katherine Champagne <kechampagne@gmail.com>
2014-01-21 01:28:34 -0500

    Remove uneeded actions from Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,6 @@
 class UsersController < ApplicationController
   before_filter :correct_user, only: [:edit, :destroy]
 
-  def index
-    @users = User.all
-    render :new
-  end
-
-  def create
-    @user = User.create(params[:user])
-    if @user.valid?
-      session[:user_id] = @user.id
-      redirect_to @user
-    else
-      @user.errors.delete(:password_digest) if @user.errors[:password_digest]
-      render :new
-    end
-  end
-
-  def new
-    @user = User.new
-  end
-
   def show
     @user = User.find(params[:id])
   end
@@ -28,7 +8,6 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-
 
   def update
     @user = User.find(params[:id])
@@ -39,17 +18,10 @@ class UsersController < ApplicationController
     end
   end
 
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to root_url
-  end
-
-
   private
 
-    def correct_user
-      redirect_to root_url unless current_user && params[:id] == current_user.id
-    end
+  def correct_user
+    redirect_to root_url unless current_user && params[:id] == current_user.id
+  end
 
 end
